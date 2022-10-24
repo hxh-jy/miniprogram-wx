cf68da2176e0f198e62f3e165987f1b2646621a2
hanxuehong <hanxuehong@excean.com>
2022-10-24 15:36:25 +0800

    事件

---
+++ components/ask/ask.js
@@ -0,0 +1,27 @@
+// components/ask/ask.js
+Component({
+    /**
+     * 组件的属性列表,存放父组件中接收的数据
+     */ 
+    properties: {
+        str: {
+            type: String,
+            value: ''
+        },
+        sonData: '我是子组件的值'
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
--- pages/ask/index.js
+++ pages/ask/index.js
@@ -4,9 +4,13 @@ Page({
     /**
      * 页面的初始数据
      */
+    tapName(e) {
+        console.log('点击事件',e)
+    },
     data: {
         test: '测试数据绑定的实现',
         isShow: true,
+        ipval: '',
         arr: [
             {
                 book: 'react',
@@ -27,47 +31,51 @@ Page({
         ],
         templateData: '通过模版传入的数据'
     },
-
+    handleInp(e) {
+        let {value} = e.detail
+        this.setData({ipval: value})
+        console.log('键盘输入时触发',this.data.ipval)
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        console.log('监听页面加载:onload')
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        console.log('监听页面初次渲染完成:onready',this)
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        console.log('监听页面显示:onshow')
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+        console.log('监听页面隐藏:onhide')
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        console.log('监听页面卸载:onUnload')
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        console.log('监听用户下拉的工作:onPullDownRefresh')
     },
 
     /**
@@ -81,6 +89,9 @@ Page({
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
+        console.log('用户点击右上角分享按钮')
+    },
+    handleSon(e) {
+        console.log('接收子组件传递过来的数据',e)
     }
 })
